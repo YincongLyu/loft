6d6102836c7c8c8b08e9339de982441f3b0f445c
Coonger <yinconglyu@foxmail.com>
2024-10-11 23:35:08 +0800

    refactor: use unique_ptr to manage r/w process

---
+++ include/file_manager.h
@@ -0,0 +1,28 @@
+// file_manager.h
+#ifndef FILE_MANAGER_H
+#define FILE_MANAGER_H
+
+#include "constants.h"
+#include <memory>
+
+namespace loft {
+
+class LogFormatTransformManager {
+public:
+    // 构造函数，初始化文件路径
+    LogFormatTransformManager(const std::string& filepath = "./data");
+
+    /*
+        读取 data 中的 第 nowTestCase 个 SQL 语句，[1, 10]
+    */
+    std::unique_ptr<char[]> readSQLN(int nowTestCase);
+
+private:
+    std::string filepath_; // 文件路径，默认是 ./data
+    const int intOffset_ = loft::INT_OFFSET; // 头部，前 4 个字节存长度
+};
+
+} // namespace loft
+
+
+#endif // FILE_MANAGER_H
--- test/fbs_test.cpp
+++ test/fbs_test.cpp
@@ -1,24 +1,27 @@
 #include "ddl_generated.h"
-#include "utils/file_manager.h"
+#include "file_manager.h"
 
 #include <gtest/gtest.h>
 #include <iostream>
 #include <vector>
+
 using namespace loft;
 
 TEST(DDLTEST, CREATE_TABLE) {
+
+    LogFormatTransformManager mgr;
     // 读二进制
-    std::vector<char> create_db_sql = utils::readSQLN(1);
+    auto create_db_sql = mgr.readSQLN(1);
 
     // 获取指向vector数据的指针
-    const char* buf = create_db_sql.data();
+    const char* buf = create_db_sql.get();
 
     // 使用GetDDL函数获取DDL对象
     const DDL* ddl = GetDDL(buf);
    
     if (ddl) {
         // 使用 check_point() 方法获取 flatbuffers::String*
-        const flatbuffers::String* checkpoint = ddl->check_point();
+        auto checkpoint = ddl->check_point();
         std::cout << checkpoint->c_str() << '\n';
     } else {
         // 处理错误情况

f7f96f9b1bfd6ef2f078a3525ff4c7b7fce9fd98
Coonger <yinconglyu@foxmail.com>
2024-10-11 22:22:03 +0800

    Initial commit: make bone

---
+++ include/utils/constants.h
@@ -0,0 +1,12 @@
+// constants.h
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace loft {
+
+constexpr int INT_OFFSET = 4;
+constexpr int SQL_SIZE_ARRAY[] = {248, 744, 3304, 3208, 3208, 2040, 2040, 1968, 264, 224};
+
+} // namespace common
+
+#endif // CONSTANTS_H

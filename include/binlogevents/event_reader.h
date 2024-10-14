147c5df9f61f2c0de0d251b0ab1b7cef4abb1160
Coonger <yinconglyu@foxmail.com>
2024-10-15 00:50:20 +0800

    fix: add event_reader.cpp

--- include/binlogevents/event_reader.h
+++ include/binlogevents/event_reader.h
@@ -12,7 +12,7 @@
 #include <string>
 #include <vector>
 #include "byteorder.h" // no used!
-// #include "wrapper_functions.h"
+#include "wrapper_functions.h"
 #include <cstring>
 
 namespace binary_log {
@@ -142,7 +142,7 @@ class Event_reader {
 
   void read_str_at_most_255_bytes(const char **destination, uint8_t *length);
 
-  uint64_t net_field_length_ll();
+  // uint64_t net_field_length_ll();
 
   
   void read_data_set(uint32_t set_len, std::list<const char *> *set);

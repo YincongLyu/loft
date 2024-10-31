e8dbfe4feca6715c768a326d897e548767c076d2
Coonger <yinconglyu@foxmail.com>
2024-11-01 00:08:44 +0800

    fix: table map event common_header write_datasize begin with construction

--- include/rows_event.h
+++ include/rows_event.h
@@ -6,7 +6,7 @@
 #define LOFT_ROWS_EVENT_H
 
 #include "abstract_event.h"
-#include "sql/field.h"
+#include "sql/mysql_fields.h"
 #include "table_id.h"
 #include <memory>
 #include <rapidjson/stringbuffer.h>
@@ -14,6 +14,18 @@
 
 class Table_map_event : public AbstractEvent {
   public:
+    Table_map_event(
+        const Table_id &tid,
+        unsigned long colcnt,
+        const char *dbnam,
+        size_t dblen,
+        const char *tblnam,
+        size_t tbllen,
+        std::vector<mysql::Field*>& column_view
+    );
+
+    ~Table_map_event() override;
+
     /** Constants representing offsets */
     enum Table_map_event_offset {
         /** TM = "Table Map" */
@@ -21,109 +33,10 @@ class Table_map_event : public AbstractEvent {
         TM_FLAGS_OFFSET = 6
     };
 
-    typedef uint16_t flag_set;
-
-    /**
-      DEFAULT_CHARSET and COLUMN_CHARSET don't appear together, and
-      ENUM_AND_SET_DEFAULT_CHARSET and ENUM_AND_SET_COLUMN_CHARSET don't
-      appear together. They are just alternative ways to pack character
-      set information. When binlogging, it logs character sets in the
-      way that occupies least storage.
-
-      SIMPLE_PRIMARY_KEY and PRIMARY_KEY_WITH_PREFIX don't appear together.
-      SIMPLE_PRIMARY_KEY is for the primary keys which only use whole values of
-      pk columns. PRIMARY_KEY_WITH_PREFIX is
-      for the primary keys which just use part value of pk columns.
-     */
-    enum Optional_metadata_field_type {
-        SIGNEDNESS = 1,  // UNSIGNED flag of numeric columns
-        DEFAULT_CHARSET, /* Character set of string columns, optimized to
-                            minimize space when many columns have the
-                            same charset. */
-        COLUMN_CHARSET,  /* Character set of string columns, optimized to
-                            minimize space when columns have many
-                            different charsets. */
-        COLUMN_NAME,
-        SET_STR_VALUE,                // String value of SET columns
-        ENUM_STR_VALUE,               // String value of ENUM columns
-        GEOMETRY_TYPE,                // Real type of geometry columns
-        SIMPLE_PRIMARY_KEY,           // Primary key without prefix
-        PRIMARY_KEY_WITH_PREFIX,      // Primary key with prefix
-        ENUM_AND_SET_DEFAULT_CHARSET, /* Character set of enum and set
-                                         columns, optimized to minimize
-                                         space when many columns have the
-                                         same charset. */
-        ENUM_AND_SET_COLUMN_CHARSET,  /* Character set of enum and set
-                                         columns, optimized to minimize
-                                         space when many columns have the
-                                         same charset. */
-        COLUMN_VISIBILITY             /* Flag to indicate column visibility
-                                         attribute. */
-    };
-
-    /**
-      Metadata_fields organizes m_optional_metadata into a structured format which
-      is easy to access. 暂时不使用
-    */
-    struct Optional_metadata_fields {
-        typedef std::pair<unsigned int, unsigned int> uint_pair;
-        typedef std::vector<std::string> str_vector;
-
-        struct Default_charset {
-            Default_charset() : default_charset(0) {}
-            bool empty() const { return default_charset == 0; }
-
-            // Default charset for the columns which are not in charset_pairs.
-            unsigned int default_charset;
-
-            /* The uint_pair means <column index, column charset number>. */
-            std::vector<uint_pair> charset_pairs;
-        };
-
-        // Contents of DEFAULT_CHARSET field are converted into Default_charset.
-        Default_charset m_default_charset;
-        // Contents of ENUM_AND_SET_DEFAULT_CHARSET are converted into
-        // Default_charset.
-        Default_charset m_enum_and_set_default_charset;
-        std::vector<bool> m_signedness;
-        // Character set number of every string column
-        std::vector<unsigned int> m_column_charset;
-        // Character set number of every ENUM or SET column.
-        std::vector<unsigned int> m_enum_and_set_column_charset;
-        std::vector<std::string> m_column_name;
-        // each str_vector stores values of one enum/set column
-        std::vector<str_vector> m_enum_str_value;
-        std::vector<str_vector> m_set_str_value;
-        std::vector<unsigned int> m_geometry_type;
-        /*
-          The uint_pair means <column index, prefix length>.  Prefix length is 0 if
-          whole column value is used.
-        */
-        std::vector<uint_pair> m_primary_key;
-        std::vector<bool> m_column_visibility;
-
-        /*
-          It parses m_optional_metadata and populates into above variables.
-
-          @param[in] optional_metadata points to the begin of optional metadata
-                                       fields in table_map_event.
-          @param[in] optional_metadata_len length of optional_metadata field.
-         */
-        Optional_metadata_fields(unsigned char *optional_metadata,
-                                 unsigned int optional_metadata_len);
-        // It is used to specify the validity of the deserialized structure
-        bool is_valid_;
-    };
-
-
-    Table_map_event(const Table_id &tid, unsigned long colcnt, const char *dbnam,
-                    size_t dblen, const char *tblnam, size_t tbllen);
-
-    ~Table_map_event() override;
-
     /** Event post header contents */
     Table_id m_table_id_;
-    flag_set m_flags;
+    typedef uint16_t flag_set;
+    flag_set m_flags = 0; // 目前的 8.0 版本默认是 0
 
     size_t m_data_size_; /** event data size */
 
@@ -141,43 +54,31 @@ class Table_map_event : public AbstractEvent {
     unsigned long m_field_metadata_size_;
     unsigned char *m_field_metadata_; /** field metadata */
     unsigned char *m_null_bits_;
-    unsigned int m_optional_metadata_len;
-    unsigned char *m_optional_metadata;
-
-    Table_map_event()
-        : AbstractEvent(TABLE_MAP_EVENT),
-        m_coltype_(nullptr),
-        m_field_metadata_size_(0),
-        m_field_metadata_(nullptr),
-        m_null_bits_(nullptr),
-        m_optional_metadata_len(0),
-        m_optional_metadata(nullptr) {}
 
     unsigned long long get_table_id() { return m_table_id_.get_id(); }
-    std::string get_table_name() { return m_tblnam_; }
-    std::string get_db_name() { return m_dbnam_; }
 
+    std::string get_table_name() { return m_tblnam_; }
+
+    std::string get_db_name() { return m_dbnam_; }
 
     // ********* impl virtual function *********************
     size_t get_data_size() override { return m_data_size_; }
+
     bool write_data_header(Basic_ostream *ostream) override;
     bool write_data_body(Basic_ostream *ostream) override;
 
-
     int save_field_metadata();
 
-// ********* log event field *********************
+    // ********* log event field *********************
 
-    rapidjson::StringBuffer m_metadata_buf_; // Metadata fields buffer
+    //    rapidjson::StringBuffer m_metadata_buf_; // Metadata fields buffer
     /**
-      Wrapper around `TABLE *m_table` that abstracts the table field set iteration logic
+      Wrapper around `TABLE *m_table` that abstracts the table field set
+      iteration logic
      */
-//    Table_columns_view<> m_column_view_{nullptr};
+    //    Table_columns_view<> m_column_view_{nullptr};
 
-    std::vector<std::unique_ptr<Field>> m_column_view_; // Table field set
+    std::vector<mysql::Field*> m_column_view_; // Table field set
 };
-
-
-
 
 #endif // LOFT_ROWS_EVENT_H
--- src/rows_event.cpp
+++ src/rows_event.cpp
@@ -2,49 +2,157 @@
 // Created by Coonger on 2024/10/21.
 //
 #include "rows_event.h"
+#include "./sql/mysql_fields.h"
 #include "abstract_event.h"
 #include "little_endian.h"
 
-Table_map_event::Table_map_event(const Table_id &tid, unsigned long colcnt, const char *dbnam,
-                                 size_t dblen, const char *tblnam, size_t tbllen): AbstractEvent(TABLE_MAP_EVENT),
-      m_table_id_(tid),
-      m_data_size_(0),
-      m_dbnam_(""),
-      m_dblen_(dblen),
-      m_tblnam_(""),
-      m_tbllen_(tbllen),
-      m_colcnt_(colcnt), /* json fields's size()*/
-      m_field_metadata_size_(0),
-      m_field_metadata_(nullptr),
-      m_null_bits_(nullptr),
-      m_optional_metadata_len(0),
-      m_optional_metadata(nullptr) {
+#include "logging.h"
 
-    if (dbnam) m_dbnam_ = std::string(dbnam, m_dblen_);
-  if (tblnam) m_tblnam_ = std::string(tblnam, m_tbllen_);
 
-  // TODO m_colcnt_, m_coltype_, m_field_metadata_size_, field_metadata_, m_null_bits, m_optional_metadata_len, m_optional_metadata 初始化
-    // m_column_view_ 初始化
-  // 1. 根据 tid 找 Table 对象
+template <typename T, typename UT>
+class Bit_stream_base {
+  protected:
+    T *m_ptr;
+    uint m_current_bit;
 
-  // 2. 处理每个 column 字段
-  // 2.1 遍历 fields 里面的每个 Field 包装成一个 Field 对象， 放入 m_column_view_
-  // 2.2.1 同时 计算得到 m_coltype
-  // 2.2.2 计算 m_null_bits_
+  public:
 
-  // 3. 得到每个 Field 的元数据
+    Bit_stream_base(T *ptr) : m_ptr(ptr), m_current_bit(0) {}
 
-  m_metadata_buf_.Reserve(1024);
-  m_field_metadata_size_ = save_field_metadata();
+    /**
+      Set the buffer pointer.
+      @param ptr Pointer where bits will be read or written.
+    */
+    void set_ptr(T *ptr) { m_ptr = ptr; }
+    /**
+      Set the buffer pointer, using an unsigned datatype.
+      @param ptr Pointer where bits will be read or written.
+    */
+    void set_ptr(UT *ptr) { m_ptr = (T *)ptr; }
 
-  // 4. 可以暂时不管 m_optinal_metadata_len, m_optional_metadata
+    /// @return the current position.
+    uint tell() const { return m_current_bit; }
+
+};
+
+/*
+ * bit 写入器
+ */
+class Bit_writer : public Bit_stream_base<char, uchar> {
+  public:
+    Bit_writer(char *ptr = nullptr) : Bit_stream_base<char, uchar>(ptr) {}
+    Bit_writer(uchar *ptr) : Bit_writer((char *)ptr) {}
+
+    /**
+      Write the next bit and move the write position one bit forward.
+      @param set_to_on If true, set the bit to 1, otherwise set it to 0.
+    */
+    void set(bool set_to_on) {
+        uint byte = m_current_bit / 8;
+        uint bit_within_byte = m_current_bit % 8;
+        m_current_bit++;
+        if (bit_within_byte == 0)
+      m_ptr[byte] = set_to_on ? 1 : 0;
+    else if (set_to_on)
+      m_ptr[byte] |= 1 << bit_within_byte;
+  }
+};
+
+Table_map_event::Table_map_event(
+    const Table_id &tid,
+    unsigned long colcnt,
+    const char *dbnam,
+    size_t dblen,
+    const char *tblnam,
+    size_t tbllen,
+    std::vector<mysql::Field*>& column_view
+)
+    : AbstractEvent(TABLE_MAP_EVENT)
+    , m_table_id_(tid)
+    , m_data_size_(0)
+    , m_dbnam_("")
+    , m_dblen_(dblen)
+    , m_tblnam_("")
+    , m_tbllen_(tbllen)
+    , m_colcnt_(colcnt)
+    , m_column_view_(column_view)
+    , /* json fields's size()*/
+    m_field_metadata_size_(0)
+    , m_field_metadata_(nullptr)
+    , m_null_bits_(nullptr) {
+
+    if (dbnam) {
+        m_dbnam_ = std::string(dbnam, m_dblen_);
+    }
+    if (tblnam) {
+        m_tblnam_ = std::string(tblnam, m_tbllen_);
+    }
+
+    m_data_size_ = TABLE_MAP_HEADER_LEN;
+
+    uchar dbuf[sizeof(m_dblen_) + 1];
+    uchar tbuf[sizeof(m_tbllen_) + 1];
+    uchar *const dbuf_end = net_store_length(dbuf, (size_t)m_dblen_);
+    assert(static_cast<size_t>(dbuf_end - dbuf) <= sizeof(dbuf));
+    uchar *const tbuf_end = net_store_length(tbuf, (size_t)m_tbllen_);
+    assert(static_cast<size_t>(tbuf_end - tbuf) <= sizeof(tbuf));
+
+    m_data_size_ +=
+        m_dblen_ + 1 + (dbuf_end - dbuf);  // Include length and terminating \0
+    m_data_size_ +=
+        m_tbllen_ + 1 + (tbuf_end - tbuf);  // Include length and terminating \0
+
+    // =========================m_column_view_ 初始化, 制作 表头==============
+    // 1. 根据 tid 找 Table 对象, 实际上不用，因为现在 tid
+    // 只是个临时值，没有实际含义，所有的信息都是从外部读取的
+    // 但是为了做调度，所以 tid 还是要记下来，不用实时给外面响应
+
+    m_coltype_ = static_cast<unsigned char *>(malloc(colcnt));
+    long pos = 0;
+    for (auto &field : m_column_view_) {
+        m_coltype_[pos++] = field->binlog_type();
+        LOG_INFO("init coltype_: field->binlog_type() = %d", field->binlog_type());
+    }
+
+    uchar cbuf[sizeof(m_colcnt_) + 1];
+    uchar *cbuf_end;
+    cbuf_end = net_store_length(cbuf, (size_t)m_colcnt_);
+    assert(static_cast<size_t>(cbuf_end - cbuf) <= sizeof(cbuf));
+    m_data_size_ += (cbuf_end - cbuf) + m_colcnt_;  // COLCNT and column types
+
+    // 3. 得到每个 Field 的元数据
+    m_field_metadata_ = static_cast<unsigned char *>(malloc(m_colcnt_ * 4));
+    memset(m_field_metadata_, 0, m_colcnt_ * 4);
+    m_field_metadata_size_ = save_field_metadata(); // 同时也填充了 m_field_metadata_
+    if (m_field_metadata_size_ < 251)
+        m_data_size_ += m_field_metadata_size_ + 1;
+    else
+        m_data_size_ += m_field_metadata_size_ + 3;
+
+/////////////////////////////
+    uint num_null_bytes = (m_colcnt_ + 7) / 8;
+    m_data_size_ += num_null_bytes;
+
+    m_null_bits_ = static_cast<unsigned char *>(malloc(num_null_bytes));
+    memset(m_null_bits_, 0, num_null_bytes);
+    Bit_writer bit_writer{this->m_null_bits_};
+
+    for (auto &field : m_column_view_) {
+        bit_writer.set(field->is_nullable());
+    }
+
+    LOG_INFO("table_map_event data size: %zu", m_data_size_);
+
+    this->common_header_ = new EventCommonHeader();
+    this->common_footer_ = new EventCommonFooter(BINLOG_CHECKSUM_ALG_OFF);
 }
 
-int Table_map_event::save_field_metadata() {
+Table_map_event::~Table_map_event() = default;
 
+
+int Table_map_event::save_field_metadata() {
     int index = 0;
-    for (auto& field_obj : m_column_view_) {
-        Field *field = field_obj.get();
+    for (auto &field : m_column_view_) {
         index += field->save_field_metadata(&m_field_metadata_[index]);
     }
 
@@ -81,14 +189,23 @@ bool Table_map_event::write_data_body(Basic_ostream *ostream) {
     uchar mbuf[2 * sizeof(m_field_metadata_size_)];
     uchar *const mbuf_end = net_store_length(mbuf, m_field_metadata_size_);
 
-    return  ostream->write(dbuf, (size_t)(dbuf_end - dbuf)) &&
-            ostream->write((const uchar *)m_dbnam_.c_str(), m_dblen_ + 1) &&
-            ostream->write(tbuf, (size_t)(tbuf_end - tbuf)) &&
-            ostream->write((const uchar *)m_tblnam_.c_str(), m_tbllen_ + 1) &&
-            ostream->write(cbuf, (size_t)(cbuf_end - cbuf)) &&
-            ostream->write(m_coltype_, m_colcnt_) &&
-            ostream->write(mbuf, (size_t)(mbuf_end - mbuf)) &&
-            ostream->write(m_field_metadata_, m_field_metadata_size_) &&
-            ostream->write(m_null_bits_, (m_colcnt_ + 7) / 8) &&
-            ostream->write((const uchar *)m_metadata_buf_.GetString(), m_metadata_buf_.GetLength());
+
+//    uchar *m_optional_metadata_ = new uchar[17];
+//    // 初始化数据
+//    uchar data[17] = {0x01, 0x02, 0x2D, 0x56, 0x02, 0x0B, 0xFC, 0xFF, 0x00,
+//                      0x05, 0x3F, 0x06, 0x3F, 0x07, 0x3F, 0x08, 0x3F};
+//    // 将数据复制到 m_optional_metadata_ 指向的内存中
+//    std::memcpy(m_optional_metadata_, data, 17);
+//    size_t m_optional_metadata_len = 17;
+
+    return ostream->write(dbuf, (size_t)(dbuf_end - dbuf))
+           && ostream->write((const uchar *)m_dbnam_.c_str(), m_dblen_ + 1)
+           && ostream->write(tbuf, (size_t)(tbuf_end - tbuf))
+           && ostream->write((const uchar *)m_tblnam_.c_str(), m_tbllen_ + 1)
+           && ostream->write(cbuf, (size_t)(cbuf_end - cbuf))
+           && ostream->write(m_coltype_, m_colcnt_)
+           && ostream->write(mbuf, (size_t)(mbuf_end - mbuf))
+           && ostream->write(m_field_metadata_, m_field_metadata_size_)
+           && ostream->write(m_null_bits_, (m_colcnt_ + 7) / 8);
+    // 最后一个 m_optional_metadata_len, m_optional_metadata 可以暂时不管
 }

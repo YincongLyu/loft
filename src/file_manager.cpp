8daca195d9f047405a498215b9b64a7f73385c1d
Coonger <yinconglyu@foxmail.com>
2024-11-01 16:47:32 +0800

    feat: transformDML func

--- src/file_manager.cpp
+++ src/file_manager.cpp
@@ -1,8 +1,15 @@
 #include "file_manager.h"
 #include "binlog.h"
+
 #include "control_events.h"
-#include "ddl_generated.h"
 #include "statement_events.h"
+#include "rows_event.h"
+#include "ddl_generated.h"
+#include "dml_generated.h"
+#include "mysql_fields.h"
+
+//#include "logging.h"
+//#include "macros.h"
 
 #include <chrono>
 #include <ctime>
@@ -13,16 +20,18 @@
 #include <fstream>
 #include <iostream>
 
+
 namespace loft {
 // ss >> std::get_time(&timeStruct, "%Y-%m-%d %H:%M:%S.%f");
 // get_time 不支持 解析微妙，只能到 秒级精度，故 拆开成两部分
 // - std::mktime：默认会将时间解析为本地时间，这样会导致时区偏移问题
 // - timegm：这个函数会将时间解释为 UTC
-unsigned long long int stringToTimestamp(const std::string& timeString) {
+unsigned long long int stringToTimestamp(const std::string &timeString) {
     std::tm timeStruct = {};
     std::string datetimePart, microsecondPart;
 
-    // Split the datetime string into two parts: "2024-08-01 14:32:41" and "000054"
+    // Split the datetime string into two parts: "2024-08-01 14:32:41" and
+    // "000054"
     size_t dotPos = timeString.find('.');
     if (dotPos != std::string::npos) {
         datetimePart = timeString.substr(0, dotPos);
@@ -49,7 +58,10 @@ unsigned long long int stringToTimestamp(const std::string& timeString) {
     auto timePointWithMicroseconds = timeSinceEpoch + additionalMicroseconds;
 
     // Convert to microseconds
-    return std::chrono::duration_cast<std::chrono::microseconds>(timePointWithMicroseconds.time_since_epoch()).count();
+    return std::chrono::duration_cast<std::chrono::microseconds>(
+               timePointWithMicroseconds.time_since_epoch()
+    )
+        .count();
 }
 
 LogFormatTransformManager::LogFormatTransformManager(const std::string &filepath
@@ -116,12 +128,19 @@ auto LogFormatTransformManager::readFileAsBinary(const std::string &filePath
     return {std::move(buffer), size};
 }
 
-void LogFormatTransformManager::transform(const char* data, unsigned long long file_sz) {
+void LogFormatTransformManager::transform(
+    const char *data, unsigned long long file_sz
+) {
     return;
 }
 
-void LogFormatTransformManager::transformDDL(const DDL *ddl, MYSQL_BIN_LOG *binLog) {
-    assert(std::strcmp(ddl->op_type()->c_str(), "DDL") == 0);
+void LogFormatTransformManager::transformDDL(
+    const DDL *ddl, MYSQL_BIN_LOG *binLog
+) {
+//    LOFT_ASSERT(
+//        std::strcmp(ddl->op_type()->c_str(), "DDL") == 0,
+//        "this is not a ddl sql"
+//    );
 
     auto ddlType = ddl->ddl_type();
 
@@ -140,55 +159,232 @@ void LogFormatTransformManager::transformDDL(const DDL *ddl, MYSQL_BIN_LOG *binL
 
     std::cout << originalCommitTs->c_str() << " " << o_ts << std::endl;
 
-    Gtid_event* ge = new Gtid_event(lastCommit, txSeq, true,
-                                    o_ts, i_ts,
-                                    original_server_version_, immediate_server_version_);
+    Gtid_event *ge = new Gtid_event(
+        lastCommit, txSeq, true, o_ts, i_ts, original_server_version_,
+        immediate_server_version_
+    );
 
     // 2. 构造 Query event
-    const char *query_arg = ddlSql->c_str();
+    const char *query_arg = ddlSql->data();
     const char *catalog_arg = nullptr;
     const char *db_arg = nullptr;
     if (dbName != nullptr) {
         db_arg = dbName->c_str();
     }
-    uint32_t query_length = strlen(query_arg);
+
+    //    uint32_t query_length = strlen(query_arg);
+    uint32_t query_length = ddlSql->size();
+//    LOG_INFO("query_: %s, query_len: %d", query_arg, query_length);
+
     unsigned long thread_id_arg = 10000;
-    unsigned long long sql_mode_arg = 0; // 随意
-    unsigned long auto_increment_increment_arg = 0; // 随意
-    unsigned long auto_increment_offset_arg = 0; // 随意
-    unsigned int number = 0;  // 一定要
+    unsigned long long sql_mode_arg = 0;             // 随意
+    unsigned long auto_increment_increment_arg = 0;  // 随意
+    unsigned long auto_increment_offset_arg = 0;     // 随意
+    unsigned int number = 0;                         // 一定要
     unsigned long long table_map_for_update_arg = 0; // 随意
     int errcode = 0;
 
-    Query_event* qe = new Query_event(query_arg, catalog_arg, db_arg, txSeq, query_length, thread_id_arg, sql_mode_arg,
-                                      auto_increment_increment_arg, auto_increment_offset_arg, number, table_map_for_update_arg, errcode);
+    Query_event *qe = new Query_event(
+        query_arg, catalog_arg, db_arg, txSeq, query_length, thread_id_arg,
+        sql_mode_arg, auto_increment_increment_arg, auto_increment_offset_arg,
+        number, table_map_for_update_arg, errcode
+    );
 
-// ******* print debug info **************************
+    // ******* print debug info **************************
     if (ddlType == nullptr) { // drop db
-        std::cout << "drop db" << std::endl;
+//        LOG_INFO("sql_type: drop db");
     } else {
         std::string sql_type = ddlType->c_str();
         if (sql_type == "CREATE TABLE") {
             if (db_arg == nullptr) { // create db
-                std::cout << "create db" << std::endl;
+//                LOG_INFO("sql_type: create db");
             } else { // create table
-                std::cout << "create table" << std::endl;
+//                LOG_INFO("sql_type: create table");
             }
         } else if (sql_type == "DROP TABLE") {
-            std::cout << "drop table" << std::endl;
+//            LOG_INFO("sql_type: drop table");
         }
     }
-// ******************************************************************
+    // ******************************************************************
+
+    Format_description_event fde(4, "8.0.26");
+    binLog->write_event_to_binlog(&fde);
 
     binLog->write_event_to_binlog(ge);
     binLog->write_event_to_binlog(qe);
+}
+
+
+enum_field_types ConvertStringType(const char *type_str) {
+    static const std::unordered_map<std::string, enum_field_types> type_map = {
+
+        {"SMALLINT", MYSQL_TYPE_SHORT}, // 2 byte
+        {"SHORT", MYSQL_TYPE_SHORT},   // 2 byte
+        {"MEDIUMINT", MYSQL_TYPE_INT24}, // 3 byte
+        {"INT", MYSQL_TYPE_LONG},       // 4 byte
+        {"BIGINT", MYSQL_TYPE_LONGLONG},  // 8 byte
+
+        {"FLOAT", MYSQL_TYPE_FLOAT},
+        {"DOUBLE", MYSQL_TYPE_DOUBLE},
+        {"DECIMAL", MYSQL_TYPE_NEWDECIMAL},
+
+        {"NULL", MYSQL_TYPE_NULL},
+        {"CHAR", MYSQL_TYPE_STRING},
+        {"VARCHAR", MYSQL_TYPE_VARCHAR},
+
+        {"TINYTEXT", MYSQL_TYPE_TINY_BLOB},
+        {"TEXT", MYSQL_TYPE_BLOB},
+        {"MEDIUMTEXT", MYSQL_TYPE_MEDIUM_BLOB},
+        {"LONGTEXT", MYSQL_TYPE_LONG_BLOB},
+
+        {"TINYBLOB", MYSQL_TYPE_TINY_BLOB},
+        {"BLOB", MYSQL_TYPE_BLOB},
+        {"MEDIUMBLOB", MYSQL_TYPE_MEDIUM_BLOB},
+        {"LONGBLOB", MYSQL_TYPE_LONG_BLOB},
+
+        {"TIMESTAMP", MYSQL_TYPE_TIMESTAMP},
+        {"DATE", MYSQL_TYPE_DATE},
+        {"TIME", MYSQL_TYPE_TIME},
+        {"DATETIME", MYSQL_TYPE_DATETIME},
+        {"YEAR", MYSQL_TYPE_YEAR},
+
+        {"BIT", MYSQL_TYPE_BIT},
+        {"ENUM", MYSQL_TYPE_ENUM},
+        {"SET", MYSQL_TYPE_SET},
+
+        {"JSON", MYSQL_TYPE_JSON},
+    };
+
+    auto it = type_map.find(type_str);
+    if (it != type_map.end()) {
+        return it->second;
+    } else {
+        return MYSQL_TYPE_INVALID;  // Return invalid type if not found
+    }
+}
+
+void LogFormatTransformManager::transformDML(
+    const DML *dml, MYSQL_BIN_LOG *binLog
+) {
+
+
+//////////****************** gtid event *************************************
+
+    auto lastCommit = dml->last_commit();
+    auto txSeq = dml->tx_seq();
+    auto immediateCommitTs = dml->msg_time();
+    auto originalCommitTs = dml->tx_time();
+    unsigned long long int i_ts = stringToTimestamp(immediateCommitTs->c_str());
+    unsigned long long int o_ts = stringToTimestamp(originalCommitTs->c_str());
+
+
+    Gtid_event *ge = new Gtid_event(
+        lastCommit, txSeq, true, o_ts, i_ts, original_server_version_,
+        immediate_server_version_
+    );
+
+    //////////****************** gtid event end *******************************
+
+    //////////****************** query event start ****************************
+    const char *query_arg = "BEGIN";  // row-based 的 DML 固定内容是 BEGIN
+    const char *catalog_arg = nullptr;
+    auto dbName = dml->db_name();
+    const char *db_arg = strdup(dbName->c_str()); // null-terminaled类型的字符串
+
+    uint32_t query_length = strlen(query_arg);
+//    LOG_INFO("query_: %s, query_len: %d", query_arg, query_length);
+
+    unsigned long thread_id_arg = 10000;
+    unsigned long long sql_mode_arg = 0;             // 随意
+    unsigned long auto_increment_increment_arg = 1;  // 随意
+    unsigned long auto_increment_offset_arg = 1;     // 随意
+    unsigned int number = 0;                         // 0 means 'en-US'
+    unsigned long long table_map_for_update_arg = 0; // 随意
+    int errcode = 0;
+
+    Query_event *qe = new Query_event(
+        query_arg, catalog_arg, db_arg, INVALID_XID, query_length, thread_id_arg,
+        sql_mode_arg, auto_increment_increment_arg, auto_increment_offset_arg,
+        number, table_map_for_update_arg, errcode
+    );
+    //////////****************** query event end ******************************
+
+    //////////****************** table map event start ************************
+
+    auto table = dml->table_();
+    const char *tbl_arg = table->c_str();
+    auto fields = dml->fields();
+
+    std::vector<mysql::Field*> field_vec;
+    size_t null_bit = 0;
+    TYPELIB *interval = new TYPELIB;
+    int idx = 0;
+    for (auto field : * fields) {
+        auto field_name = field->name();
+        auto fieldMeta = field->meta();
+        auto field_length = fieldMeta->length();
+        bool is_unsigned = fieldMeta->is_unsigned();
+        bool is_nullable = fieldMeta->nullable();
+        auto data_type = fieldMeta->data_type(); // 根据 这里的类型，构建 对应的 Field 对象
+        auto decimals = fieldMeta->precision();
+
+        enum_field_types field_type =  ConvertStringType(data_type->c_str());
+        if (field_type == MYSQL_TYPE_ENUM || field_type == MYSQL_TYPE_SET) {
+            interval->count = field_length;
+        }
+
+        if (is_nullable) {
+            null_bit = idx;
+        }
+
+        mysql::Field *field_obj = mysql::make_field(field_name->c_str(), field_length, is_unsigned, is_nullable, null_bit,
+                                          field_type, interval, decimals);
+
+        field_vec.emplace_back(field_obj);
+    }
+
+    Table_id tid(13); // 暂时随便写一个，实际上要做一个 连续的 id 分配器
+    unsigned long colcnt = field_vec.size();
+    Table_map_event *table_map_event = new Table_map_event(tid, colcnt, db_arg, strlen(db_arg), tbl_arg, strlen(tbl_arg), field_vec);
+
+    //////////****************** table map event end *************************
+
+
+    //////////****************** rows event start ****************************
+    // insert 没有 keys 要判断一下
+    auto keys = dml->keys();
+
+    if (keys) {
+//        LOG_INFO("current sql is update or delete");
+    }
+
+    auto newData = dml->new_data();
+    auto opType = dml->op_type();
+
+    //////////****************** rows event end ****************************
+
+    //////////****************** xid event start ******************************
+
+    Xid_event *xe = new Xid_event(txSeq);
+
+    //////////****************** xid event end ******************************
+
+
+    Format_description_event fde(4, "8.0.26");
+    binLog->write_event_to_binlog(&fde);
+
+//    binLog->write_event_to_binlog(ge);
+
+    // TODO 这两个 query log event 有问题！mysqlbinlog 解释不出来
+//    binLog->write_event_to_binlog(qe);
+    binLog->write_event_to_binlog(table_map_event);
+//    binLog->write_event_to_binlog(rows_event);
+//    binLog->write_event_to_binlog(xe);
 
 }
 
-void LogFormatTransformManager::transformDML(const DML *dml, MYSQL_BIN_LOG *binLog) {
 
 
 
-}
 
 } // namespace loft
